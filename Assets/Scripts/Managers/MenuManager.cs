6324b5896de1c3229d37d6117adf98048c2362a2
Ugi <ugurboranbusiness@gmail.com>
2025-12-28 20:32:52 +0300

    MainMenu Canvas, AudioManager & Audio Settings Addition

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,27 +1,53 @@
-﻿// MenuManager.cs
-using Unity.VisualScripting;
-using UnityEditor.SearchService;
+﻿// MenuManager.cs - GÜNCELLENMİŞ
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Oyunu başlat - Play butonu çağırır
-    public void PlayGame()
+    public GameObject settingsPanel;
+    public Toggle soundToggle;
+
+    // Oyun başladığında çalışır
+    void Start()
     {
-        // GameScene sahnesini yükle
-        SceneManager.LoadScene("GameScene");
+        // Toggle durumunu güncelle
+        UpdateToggleState();
     }
-    
-    // Ayarlar menüsünü aç (şimdilik boş)
+
+    // Ayarlar menüsü her açıldığında toggle durumunu güncelle
     public void OpenSettings()
     {
-        Debug.Log("Ayarlar açılacak");
-        // TODO: Ayarlar paneli oluşturulacak
+        // Settings panelini göster
+        settingsPanel.SetActive(true);
+
+        // Toggle durumunu güncelle
+        UpdateToggleState();
     }
-    
-    // Oyundan çık (Android'de kullanılmaz)
+
+    // Toggle durumunu AudioManager'dan al ve güncelle
+    void UpdateToggleState()
+    {
+        if (AudioManager.Instance != null)
+        {
+            // OnValueChanged event'ini tetiklememek için
+            // önce listener'ı kaldır, sonra tekrar ekle
+            soundToggle.onValueChanged.RemoveAllListeners();
+            soundToggle.isOn = AudioManager.Instance.IsSoundOn();
+            soundToggle.onValueChanged.AddListener(AudioManager.Instance.ToggleSound);
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Oyun kapatılıyor");
