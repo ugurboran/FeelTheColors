5ed28409f1bb3b70fb8bc4a54d9dbb4973877bef
Ugi <ugurboranbusiness@gmail.com>
2026-01-12 16:40:36 +0300

    Main Menu Image Addition & Rotation

---
+++ Assets/Scripts/Effects/RotateImage.cs
@@ -0,0 +1,27 @@
+﻿// RotateImage.cs
+using UnityEngine;
+
+public class RotateImage : MonoBehaviour
+{
+    // Dönüş hızı (derece/saniye)
+    public float rotationSpeed = 20f;
+
+    // Saat yönünde mi tersi yönde mi
+    public bool clockwise = true;
+
+    // Her frame çalışır
+    void Update()
+    {
+        // Dönüş miktarını hesapla
+        float rotation = rotationSpeed * Time.deltaTime;
+
+        // Eğer saat yönü tersiyse negatif yap
+        if (!clockwise)
+        {
+            rotation = -rotation;
+        }
+
+        // Z ekseninde döndür
+        transform.Rotate(0f, 0f, rotation);
+    }
+}
